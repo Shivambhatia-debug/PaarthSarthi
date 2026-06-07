83d345a60f3d6a1db1c54e1daeb4c6aa70a9632e
Shivam Raj <195776198+Shivambhatia-debug@users.noreply.github.com>
2026-06-08 04:41:51 +0530

    Refactor and configure config.js

--- mobile/src/constants/config.js
+++ mobile/src/constants/config.js
@@ -3,14 +3,14 @@
 // For iOS simulator: http://localhost:5000
 // For physical device: use your computer's local IP (e.g., http://192.168.x.x:5000)
 
-const DEV_API_URL = 'http://10.185.221.68:5000';
+const DEV_API_URL = 'http://192.168.1.7:5000';
 const PROD_API_URL = 'https://paarthsarthi.vercel.app'; 
 
 const isDev = __DEV__;
 
 export default {
-  API_BASE_URL: isDev ? DEV_API_URL : PROD_API_URL,
-  API_URL: isDev ? `${DEV_API_URL}/api` : `${PROD_API_URL}/api`,
+  API_BASE_URL: PROD_API_URL,
+  API_URL: `${PROD_API_URL}/api`,
   APP_NAME: 'ParthSarthi',
   APP_TAGLINE: 'Accelerate your career',
   VERSION: '1.0.0',
